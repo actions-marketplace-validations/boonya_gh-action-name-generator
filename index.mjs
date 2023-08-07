d6798db7c57dfca0f085382026e37fbd6ee692ef
Serhii [boonya] Buinytskyi <779184+boonya@users.noreply.github.com>
2023-08-07 17:28:38 +0200

    default values + descrptions

--- index.mjs
+++ index.mjs
@@ -1,14 +1,18 @@
-const core = require('@actions/core')
-const { uniqueNamesGenerator, adjectives, colors, animals, names } = require('unique-names-generator')
+import core from '@actions/core';
+import { uniqueNamesGenerator, adjectives, colors, animals, names } from 'unique-names-generator';
 
 try {
     const config = {
-        dictionaries: [adjectives, colors, animals, names],
         separator: core.getInput('separator'),
         length: core.getInput('length'),
-        style: core.getInput('style')
+        style: core.getInput('style'),
+        seed: core.getInput('seed'),
     }
-    const output = uniqueNamesGenerator(config)
+    console.log(`Options: ${JSON.stringify(config)}`)
+    const output = uniqueNamesGenerator({
+      dictionaries: [adjectives, colors, animals, names],
+      ...config
+    })
     console.log(`Generated name: ${output}`)
     core.setOutput('generated_name', output)
     core.exportVariable('generated_name', output)
